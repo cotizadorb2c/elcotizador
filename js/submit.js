c18309281d218067098e5c84d28906a6d597a4a4
cotizadorb2c <72567540+cotizadorb2c@users.noreply.github.com>
2020-10-09 11:55:34 -0500

    Línea Base El Cotizador

---
+++ js/submit.js
@@ -0,0 +1,12 @@
+function validateForm() {
+
+            submitted=true;
+            return true;
+          
+        }
+
+function clearData() {
+            document.getElementById('correo').value = "";
+            document.getElementById('nombre').value = "";
+            document.getElementById('telefono').value = "";
+        }
